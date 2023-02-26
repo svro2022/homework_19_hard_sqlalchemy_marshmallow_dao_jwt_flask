56a4ef9c0de4079740a9d472250cc3947f14576e
svro <ichistrawberry2012@gmail.com>
2023-02-26 20:20:02 +0300

    working with project hard source

---
+++ views/directors.py
@@ -0,0 +1,48 @@
+from flask_restx import Resource, Namespace
+from flask import request
+
+from dao.model.director import DirectorSchema
+from implemented import director_service
+from utils import auth_required
+from utils import admin_required
+
+director_ns = Namespace('directors')
+
+
+@director_ns.route('/')
+class DirectorsView(Resource):
+    @auth_required
+    def get(self):
+        rs = director_service.get_all()
+        res = DirectorSchema(many=True).dump(rs)
+        return res, 200
+
+    @admin_required
+    def post(self):
+        data = request.json
+        director_service.create(data)
+        return "", 201
+
+
+@director_ns.route('/<int:rid>')
+class DirectorView(Resource):
+    @auth_required
+    def get(self, rid):
+        r = director_service.get_one(rid)
+        sm_d = DirectorSchema().dump(r)
+        return sm_d, 200
+
+    @admin_required
+    def put(self, rid):
+        data = request.json
+        if 'id' not in data:
+            data['id'] = rid
+        director_service.update(data)
+        return "", 204
+
+
+    @admin_required
+    def delete(self, rid):
+        director_service.delete(rid)
+        return "", 204
+
